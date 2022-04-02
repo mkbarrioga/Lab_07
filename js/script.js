c96532b8c3e522c1aa505f20bccce1b650a77093
Marc Kevin <mbarrioga@hawk.iit.edu>
2022-04-02 14:52:34 -0500

    Update script.js
    added clickFunction to show a window alert and as well as change an element's innerHTML to produce an output.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 function init(){
-  //add
+  document.getElementById('entrybutton').addEventListener('click', clickFunction)
+
+  function clickFunction(){
+    var message = document.getElementById('entryinput').value;
+    alert('Marc Kevin Barrioga: ' + message);
+    document.getElementById('textoutput').innerHTML = message;
+  }
 }
 
 window.addEventListener('load', init);
